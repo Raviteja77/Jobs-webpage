e87f38ec8fb525d8b5644db309cada81cd8b2942
Raviteja <geddadaraviteja612@gmail.com>
2023-05-04 21:06:08 -0400

    Added comments and changed functionality in home page to render without errors when user is not logged in

--- backend_app/app/__init__.py
+++ backend_app/app/__init__.py
@@ -4,29 +4,37 @@ from config import config
 from flask_login import LoginManager
 from os import path
 
+# create a SQLAlchemy object
 db = SQLAlchemy()
+
+# set the name of the database
 DB_NAME = "jobs_webapp.db"
 
 def create_app(config_name):
+    # create a Flask app
     app = Flask(__name__)
+    
+    # load configuration from config.py
     app.config.from_object(config[config_name])
+    
+    # initialize the configuration
     config[config_name].init_app(app) 
 
+    # initialize the SQLAlchemy object with the Flask app
     db.init_app(app)
 
+    # import and register the main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
+    # create the database tables
     with app.app_context():
         db.create_all()
 
+    # initialize the LoginManager for user authentication
     login_manager = LoginManager()
     login_manager.login_view = 'main.login'
     login_manager.init_app(app)
     
+    # return the Flask app
     return app
-
-
-def create_database(app):
-    if not path.exists('app/' + DB_NAME):
-        db.create_all(app=app)
--- frontend_app/app/__init__.py
+++ frontend_app/app/__init__.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -5,32 +6,41 @@ from config import config
 from flask_login import LoginManager
 from os import path
 
+# Initialize SQLAlchemy, set database name and Bootstrap
 db = SQLAlchemy()
 DB_NAME = "jobs_webapp.db"
 bootstrap = Bootstrap()
 
+# Create a Flask app
 def create_app(config_name):
     app = Flask(__name__)
+
+    # Load configuration from config.py file
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    # Initialize the database and Bootstrap
     db.init_app(app)
     bootstrap.init_app(app)
 
+    # Register the main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # Import User model and create all tables
     from .models import User
-    
     with app.app_context():
         db.create_all()
 
+    # Initialize the LoginManager and set the login view
     login_manager = LoginManager()
     login_manager.login_view = 'main.login'
     login_manager.init_app(app)
 
-    return app
+    # Load the user by ID
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
-def create_database(app):
-    if not path.exists('app/' + DB_NAME):
-        db.create_all(app=app)
+    # Return the Flask app
+    return app
